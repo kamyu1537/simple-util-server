69ad5698b98dcecc69fb48835ed92a5128647fd6
Byeon Seongun <kamyu1537@gmail.com>
2022-10-29 17:26:47 +0900

    GET으로 가져올수 있게끔 코드 변경

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const http = require('http');
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+const checkDir = () => {
+  const dir = path.join(__dirname, 'files');
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+};
+
+checkDir();
 
 const server = http.createServer((req, res) => {
   const url = new URL(req.url, 'http://' + req.headers.host);
@@ -7,14 +19,45 @@ const server = http.createServer((req, res) => {
     case '/base2png':
       if (req.method === 'POST') {
         const chunk = [];
-        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         req.on('data', (data) => {
           chunk.push(data);
         });
         req.on('end', () => {
-          res.write(Buffer.from(Buffer.concat(chunk).toString(), 'base64'));
+          const id = Date.now() + '_' + crypto.randomUUID();
+
+          const filePath = path.join(__dirname, 'files', id + '.png');
+          fs.writeFile(filePath, Buffer.from(Buffer.concat(chunk).toString(), 'base64'), (err) => {
+            if (err) {
+              res.end('error');
+            } else {
+              res.end(id);
+            }
+          });
+
+          setTimeout(() => {
+            fs.rmSync(filePath);
+          }, 1000 * 60 * 5);
+
+          res.write(id);
           res.end();
         });
+      } else if (req.method === 'GET') {
+        const id = url.searchParams.get('id');
+        if (!id) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('id is required');
+          return;
+        } else {
+          const filePath = path.join(__dirname, 'files', id + '.png');
+          if (fs.existsSync(filePath)) {
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            fs.createReadStream(filePath).pipe(res);
+          } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('not found');
+          }
+        }
       } else {
         res.statusCode = 405;
         res.end();
@@ -25,6 +68,25 @@ const server = http.createServer((req, res) => {
       res.end();
   }
 });
+
+setInterval(() => {
+  try {
+    // remove old files
+    const dir = path.join(__dirname, 'files');
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stats = fs.statSync(filePath);
+      const now = Date.now();
+      const diff = now - stats.mtimeMs;
+      if (diff > 1000 * 60 * 5) {
+        fs.rmSync(filePath);
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}, 1000 * 60 * 30);
 
 server.listen(3000, '0.0.0.0', () => {
   console.log('Server is running on', server.address());
